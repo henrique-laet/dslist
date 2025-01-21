d27b6534aa018a8e98842203900e946d20a7cf8e
henrique <henriquebaslaet@hotmail.com>
2025-01-21 14:38:41 -0300

    Domain Model

---
+++ src/main/java/com/devsuperior/dslist/entities/BelongingPk.java
@@ -0,0 +1,56 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import org.hibernate.annotations.ManyToAny;
+
+import java.util.Objects;
+
+@Embeddable
+public class BelongingPk {
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    @ManyToOne
+    @JoinColumn(name = "list_id")
+    private GameList list;
+
+    public BelongingPk(){
+    }
+
+    public BelongingPk(Game game, GameList list) {
+        this.game = game;
+        this.list = list;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public GameList getList() {
+        return list;
+    }
+
+    public void setList(GameList list) {
+        this.list = list;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BelongingPk that)) return false;
+        return Objects.equals(getGame(), that.getGame()) && Objects.equals(getList(), that.getList());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getGame(), getList());
+    }
+}
